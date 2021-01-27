d7f30d8e327c54f4f0a160f31d68d4a1ec602ddc
Jonathan David Estilo <jonathan.estilo@canon-europe.com>
2021-01-28 02:17:48 +0800

    Set cache duration to 1 day

--- src/config.ts
+++ src/config.ts
@@ -7,5 +7,5 @@ export default {
     privateKey: process.env.MARVEL_API_PRIVATE_KEY ?? "",
     rootUrl: process.env.MARVEL_API_ROOT_URL ?? "http://gateway.marvel.com",
     charactersRelativeUrl: process.env.MARVEL_API_CHARACTERS_RELATIVE_URL ?? "/v1/public/characters",
-    requestCacheDuration: +(process.env.REQUEST_CACHE_DURATION ?? 172800) // 2 days
+    requestCacheDuration: +(process.env.REQUEST_CACHE_DURATION ?? 86400) // 1 day
 };
